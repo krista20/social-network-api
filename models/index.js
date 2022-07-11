de63416ca3debc415fbba62da4dfbc32978f3b5e
Rush <kriscookies20@gmail.com>
2022-07-10 22:43:06 -0700

    fixing errors

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 // Retrieve all notes
 app.get('/thought', (req, res) => {
     db.Note.find({})
